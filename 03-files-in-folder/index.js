ea0e4bde90ebb95aabe656164e0eb3936855d1a6
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-01-23 07:14:45 +0500

    fix: fix getting file stat

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 const fsPromise = require('fs/promises');
 const secretPath = path.join(__dirname, 'secret-folder');
 
@@ -11,7 +10,8 @@ async function parseFiles() {
         const indexOfPoint = file.name.indexOf('.');
         const fileName = file.name.slice(0, indexOfPoint);
         const fileExtension = file.name.slice(indexOfPoint + 1);
-        const fileSizeByte = (await getFileStat(file)).size;
+        const stats = await fsPromise.stat(path.join(file.path, file.name));
+        const fileSizeByte = stats.size;
         const fileSize = fileSizeByte / 1000;
         console.log(`${fileName} - ${fileExtension} - ${fileSize}kb`);
       }
@@ -21,15 +21,3 @@ async function parseFiles() {
   }
 }
 parseFiles();
-
-function getFileStat(file) {
-  return new Promise((resolve, reject) => {
-    fs.stat(path.join(file.path, file.name), (err, stats) => {
-      if (err) {
-        console.error(err.message);
-        return reject(err);
-      }
-      return resolve(stats);
-    });
-  });
-}
