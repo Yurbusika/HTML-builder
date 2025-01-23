432e3a5acbdda75fb3248c065b845bfc43dba132
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-01-23 06:48:40 +0500

    feat: add terminate process from keyword

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,10 +6,16 @@ const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 stdout.write('Hello! Please enter your message:\n');
 stdin.on('data', (chunk) => {
+  const strChunk = chunk.toString().trim();
+
+  if (strChunk === 'exit') {
+    stdout.write('Goodbye!\n');
+    process.exit(1);
+  }
   output.write(chunk);
 });
 
 process.on('SIGINT', () => {
-  stdout.write('Goodbye!');
+  stdout.write('\nGoodbye!\n');
   process.exit(1);
 });
