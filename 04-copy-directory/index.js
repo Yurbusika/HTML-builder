486f93f5d78d4d471b7e29a6f70c30c66ff8a841
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-01-23 09:38:27 +0500

    feat: add support for actual state

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,13 +8,26 @@ async function copyDir() {
     const pathCopyDir = path.join(__dirname, 'files-copy');
 
     await fsPromise.mkdir(pathCopyDir, { recursive: true });
+    const copyFiles = await fsPromise.readdir(pathCopyDir, {
+      withFileTypes: true,
+    });
 
-    const files = await fsPromise.readdir(pathSrcDir, { withFileTypes: true });
+    const files = await fsPromise.readdir(pathSrcDir, {
+      withFileTypes: true,
+    });
 
     for (const file of files) {
       if (file.isFile()) {
         const copyFileSrc = path.join(pathSrcDir, file.name);
         const copyFileDest = path.join(pathCopyDir, file.name);
+
+        for (const copyFile of copyFiles) {
+          if (!files.some((file) => file.name === copyFile.name)) {
+            await fsPromise.unlink(
+              path.join(__dirname, 'files-copy', copyFile.name),
+            );
+          }
+        }
 
         fs.copyFile(
           copyFileSrc,
