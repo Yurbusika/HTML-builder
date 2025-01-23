b4df0959026770153a5b6eb3173528dab8756a2e
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-01-24 01:51:21 +0500

    feat: add support actual state

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,7 +4,7 @@ const fsPromise = require('fs/promises');
 async function mergeStyles() {
   try {
     const styles = await fsPromise.readdir(path.join(__dirname, 'styles'));
-
+    let bundle = '';
     for (const styleFile of styles) {
       const indexOfPoint = styleFile.indexOf('.');
       const fileExtension = styleFile.slice(indexOfPoint + 1);
@@ -12,11 +12,12 @@ async function mergeStyles() {
         const data = await fsPromise.readFile(
           path.join(__dirname, 'styles', styleFile),
         );
-        fsPromise.appendFile(
-          path.join(__dirname, 'project-dist', 'bundle.css'),
-          data,
-        );
+        bundle += data + '\n';
       }
+      fsPromise.writeFile(
+        path.join(__dirname, 'project-dist', 'bundle.css'),
+        bundle,
+      );
     }
   } catch (err) {
     console.error(err);
